ef706d50a9af3e20765b3cc252d3e39a21d4d661
valdirjunior90 <slipkinoot@hotmail.com>
2021-03-13 17:05:15 -0300

    Questão 10 reformulada

--- exercicio10.java
+++ exercicio10.java
@@ -10,10 +10,25 @@ class Entre {
         num = leitura.nextInt();
         leitura.nextLine();
         
-        if(num >= 1 && num <= 5){
-            System.out.println(num);
-        }else{
-            System.out.println("Numero inválido!");
+        switch(num){
+            case 1:
+                System.out.println("Um");
+                break;
+            case 2:
+                System.out.println("Dois");
+                break;
+            case 3: 
+                System.out.println("Três");
+                break;
+            case 4: 
+                System.out.println("Quatro");
+                break;
+            case 5: 
+                System.out.println("Cinco");
+                break;
+            default:
+                System.out.println("Numero Inválido!");
+                break;
         }
    } 
 }
