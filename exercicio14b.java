8d94be113eecf41eda48c538409175e82c1771b6
valdirjunior90 <slipkinoot@hotmail.com>
2021-03-16 19:48:30 -0300

    Questão 14 pronta

---
+++ exercicio14b.java
@@ -0,0 +1,28 @@
+//Verificar se a senha, informada durante a execução, é correta. Quando a senha estiver correta, exiba “senha correta” e o programa é encerrado.
+//Quando a senha estiver errada, exiba “senha incorreta”, e permita a entrada novamente da senha, repetindo esse processo até que a senha entrada
+//seja correta.
+//a) Resolva esse problema utilizando a estrutura do-while. 
+import java.util.Scanner;
+
+class Senha2 {
+    static Scanner read = new Scanner(System.in);
+    public static void main(String[] args) {
+        
+        String a, senha;
+        int enter;
+        
+        senha = "oi";
+        enter = 1;
+        do{
+            System.out.println("Digite a senha: ");
+            a = (String) read.nextLine();
+
+            if(a.equals(senha) == true){
+                System.out.println("Senha correta!");
+                enter = 0;
+            }else{
+                System.out.println("Senha Incorreta, tente novamente");
+            }
+        }while(enter == 1);
+    }
+};
